50a31d3fdff69181a3498989a37af345cc8675be
bluefirerose <rose.a.exile@gmail.com>
2023-06-27 01:27:36 +0800

    fixed the html, php and database connection

--- api/config.php
+++ api/config.php
@@ -1,16 +1,15 @@
-<!-- store your database credentials and establish a database connection. -->
 
 <?php
 
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "kodego_db"
+$dbname = "kodego_db";
 
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 if($conn->connect_error){
-    die("Connection error:" .$conn -> connect_error);
+    die("Connection error:" . $conn -> connect_error);
 
 }
 echo "Connected successfully";
